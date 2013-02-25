a62779c5a6987700a8a0cd7ffde2f37c5c6cfcad
Roderik van der Veer <roderik@vanderveer.be>
2013-02-25 21:48:52 +0100

    Tweak metadata

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,7 @@ maintainer_email  "roderik@vanderveer.be"
 license           "Apache 2.0"
 description       ""
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0"
-supports          "ubuntu"
+version           "0.0.1"
 
 depends "apache2"
 depends "git"
